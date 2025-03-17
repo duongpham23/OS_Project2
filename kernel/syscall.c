7fa0bfcecfa939786b2e9554e53099086dc833e2
Lưu Ngô Quốc Bảo <23127327@student.hcmus.edu.vn>
2025-03-17 23:59:33 +0700

    Add files via upload

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -128,6 +128,33 @@ static uint64 (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_sysinfo]   sys_sysinfo,
+[SYS_trace] sys_trace
+};
+
+static char *syscall_names[] = {
+  [SYS_fork]    "fork",
+  [SYS_exit]    "exit",
+  [SYS_wait]    "wait",
+  [SYS_pipe]    "pipe",
+  [SYS_read]    "read",
+  [SYS_kill]    "kill",
+  [SYS_exec]    "exec",
+  [SYS_fstat]   "fstat",
+  [SYS_chdir]   "chdir",
+  [SYS_dup]     "dup",
+  [SYS_getpid]  "getpid",
+  [SYS_sbrk]    "sbrk",
+  [SYS_sleep]   "sleep",
+  [SYS_uptime]  "uptime",
+  [SYS_open]    "open",
+  [SYS_write]   "write",
+  [SYS_mknod]   "mknod",
+  [SYS_unlink]  "unlink",
+  [SYS_link]    "link",
+  [SYS_mkdir]   "mkdir",
+  [SYS_close]   "close",
+  [SYS_sysinfo] "sysinfo",
+  [SYS_trace]   "trace"
 };
 
 void
@@ -140,10 +167,24 @@ syscall(void)
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     // Use num to lookup the system call function for num, call it,
     // and store its return value in p->trapframe->a0
-    p->trapframe->a0 = syscalls[num]();
+    int return_value = syscalls[num]();
+    p->trapframe->a0 = return_value;
+
+    if (p->trace_mask & (1 << num))
+    {
+      printf("%d: syscall %s -> %d\n", p->pid, syscall_names[num], return_value);
+    }
   } else {
     printf("%d %s: unknown sys call %d\n",
             p->pid, p->name, num);
     p->trapframe->a0 = -1;
   }
+}
+
+uint64 sys_trace(void)
+{
+    int mask;
+    argint(0, &mask);
+    myproc()->trace_mask = mask;
+    return 0;
 }
